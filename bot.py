fde5e905d570e733cfdbd7b7cb27001a38476775
Yushima <yuhu198275@gmail.com>
2020-12-01 20:53:31 +0800

    image fucation ++

---
+++ bot.py
@@ -0,0 +1,66 @@
+import discord
+from discord.ext import commands
+import random
+import array
+import os
+import os.path
+
+bot = commands.Bot(command_prefix='!')
+mylist = os.listdir("./image")
+
+    
+@bot.event
+async def on_ready():
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    channel = bot.get_channel(783286348144443404)
+    await channel.send('少女讀取中...')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.author.bot: 
+        return
+
+    with open('words/gumLee.txt', 'r' , encoding="utf-8")  as file:
+        for line in file:
+            if line.strip() in message.content:
+                image = random.choice(mylist)
+                await message.channel.send("咁你有咩高見" )
+                await message.channel.send(file=discord.File("image/" + image))
+                return
+    await bot.process_commands(message)
+
+@bot.command()
+async def add(ctx , left : int, right : int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def roll(ctx, dice : str):
+    print('------')
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.group(pass_context=True)
+async def cool(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+@bot.command(name='bot')
+async def _bot(ctx):
+    await ctx.send('the bot is cool.')
+
+@bot.command(name='殺菇')
+async def mushroom(ctx):
+    await ctx.send('我要殺菇!')
+
+
+bot.run('NzgzMjczMTg1MTkyOTY4MTky.X8YV_w.cOvifl796xVCA2L1P6dn3Dr67D4')
