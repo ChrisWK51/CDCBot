2240f61bcf072aa069aab5838dda95105adfd31c
Yushima <yuhu198275@gmail.com>
2020-12-02 09:52:21 +0800

    Small fix on photo and message

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ day = {
     "Sun": "星期日",
 }
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!' , description="殺菇咩整出黎既殺菇咩bot \n 依家呢個殺菇bot有既command")
 goldList = os.listdir("./image/goldImage")
 sagumelist = os.listdir("./image/Sagume")
 on9WaterList = os.listdir("./image/on9Water")
@@ -45,10 +45,10 @@ async def on_message(message):
     for i in jdata["goldenText"]:
         if i in message.content:
             image = random.choice(goldList)
-            await message.channel.send("咁你有咩高見" )
-            await message.channel.send(file=discord.File("image/goldImage/" + image))
+            await message.channel.send("咁你有咩高見" , file=discord.File("image/goldImage/" + image))
             return
     for i in jdata["rubbishWater"]:
+        msg = message.content.str.lower()
         if i in message.content:
             image = random.choice(on9WaterList)
             await message.channel.send(file=discord.File("image/on9Water/" + image))
@@ -63,7 +63,7 @@ async def on_message(message):
 
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title="殺菇bot", description=jdata["infoDescription"], color=0xC6CFD6)
+    embed = discord.Embed(title=bot.user.name, description=bot.description, color=0xC6CFD6)
     times = len(jdata["Command"])
     for i in range(times):
         embed.add_field(name=jdata["Command"][i], value=jdata["Use"][i], inline=False)
@@ -97,15 +97,17 @@ async def cool(ctx):
 
 @bot.command(name='bot')
 async def _bot(ctx):
-    await ctx.send('殺菇咩好耶 殺菇咩又中')
+    
     image = random.choice(sagumelist)
-    await ctx.send(file=discord.File("image/Sagume/" + image))
+    await ctx.send('殺菇咩好耶 殺菇咩又中',file=discord.File("image/Sagume/" + image))
+  
 
 @bot.command(name='殺菇')
 async def mushroom(ctx ):
-    await ctx.send('我要殺菇!')
+    
     image = random.choice(sagumelist)
-    await ctx.send(file=discord.File("image/Sagume/" + image))
+    await ctx.send('我要殺菇!' , file=discord.File("image/Sagume/" + image))
+    
     return
 
 @bot.command()
@@ -118,6 +120,6 @@ async def time(ctx):
     
     week = day[time.strftime('%a')]
     localTime = time.strftime(f'%Y年%m月%d日 {week} %X (GMT+8) ')
-    await ctx.send(f"香港依家既時間係 : {localTime}")
+    await ctx.send(f"月都依家既時間係 : {localTime}")
 
 bot.run(jdata["TOKEN"])
