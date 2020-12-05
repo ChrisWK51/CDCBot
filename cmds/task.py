fecb0667ae3e595d226f9e4e09fe917fdab8759b
Yushima <yuhu198275@gmail.com>
2020-12-05 15:21:29 +0800

    .

--- cmds/task.py
+++ cmds/task.py
@@ -3,11 +3,11 @@ from discord.ext import commands
 from  core.classes import Cog_Extension
 import json,asyncio,datetime
 from pytz import timezone as pt
-
+import urllib.request
 class Task(Cog_Extension):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.counter = 0 
+        ##self.counter = 0 
         #async def interval():
         #    await self.bot.wait_until_ready()
         #    self.channel = self.bot.get_channel(783286397632380928)
@@ -22,10 +22,10 @@ class Task(Cog_Extension):
               now_time = pt('HongKong').fromutc(datetime.datetime.now()).strftime('%H%M')
               with open('setting.json','r' , encoding="utf-8") as jFile:
                   jdata = json.load(jFile)
-              if now_time == jdata['time'] and self.counter==0 :          
-                  await self.channel.send('Task Working')
-                  self.counter = 1
-                  await asyncio.sleep(1)
+              if now_time == jdata['time']  :          
+                  await self.channel.send(getTodayWeather())
+                  ##self.counter = 1
+                  await asyncio.sleep(60)
               else:
                   await asyncio.sleep(1)
                   pass
@@ -42,13 +42,22 @@ class Task(Cog_Extension):
         if int(time) > 2359 or int(time) < 0:
               await ctx.send("用錯左啊 收皮啦")
               return 
-        self.counter = 0 
+        ##self.counter = 0 
         with open('setting.json','r' , encoding="utf-8") as jFile:
             jdata = json.load(jFile)
         jdata['time'] = time
         with open('setting.json','w' , encoding="utf-8") as jFile:
             json.dump(jdata,jFile,indent=4)
         await ctx.send(f"Time successfully set")
+
+def getTodayWeather():
+    URL = 'http://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=tc'
+    Result = urllib.request.urlopen(URL).read().decode()
+    Result = json.loads(Result)
+    ReturnString ="各位早晨  以下係殺菇咩帶俾你哋嘅天氣預報\n"
+    for weather in Result:
+        ReturnString += Result[weather] + "\n"
+    return ReturnString
 
 def setup(bot):
     bot.add_cog(Task(bot))
